f105c8b97763a0520a8fe2b90e21eebffd1fb406
masudayouko <youyo1204@gmail.com>
2020-08-24 09:55:44 +0900

    fix top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,7 @@ $(document).on('turbolinks:load', function() {
     // スライドショーの変化 ("fade" or "slide")
     transition : 'fade',
     // 変化に係る時間(ミリ秒)
-    speed : 200,
+    speed : 150,
     // easingの種類
     easing : 'easeOutQuart',
     // ナビゲーションの形("block" or "bubble")
@@ -34,7 +34,7 @@ $(document).on('turbolinks:load', function() {
     // スライドショーの自動再生(falseで自動再生なし)
     autoPlay : true,
     // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 1800,
+    autoPlayDuration : 1300,
     // キーボードの矢印キーによるスライド送りの設定(trueで有効)
     keyboardOnAlways : false,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
